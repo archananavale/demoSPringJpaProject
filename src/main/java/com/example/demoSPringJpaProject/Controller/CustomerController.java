c416ba89e0223a2887eaed8d3d8303c408d73b98
Archana Ramnath Ramnath Navale <archana.navale49@gmail.com>
2021-10-08 00:07:02 +0200

    Junit test case added.

--- src/main/java/com/example/demoSPringJpaProject/Controller/CustomerController.java
+++ src/main/java/com/example/demoSPringJpaProject/Controller/CustomerController.java
@@ -4,7 +4,6 @@ import com.example.demoSPringJpaProject.Model.Customer;
 import com.example.demoSPringJpaProject.Repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Map;
 
--- src/test/java/com/example/demoSPringJpaProject/DemoSPringJpaProjectApplicationTests.java
+++ src/test/java/com/example/demoSPringJpaProject/DemoSPringJpaProjectApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.demoSPringJpaProject;
 
+import com.example.demoSPringJpaProject.Model.Employee;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class) //Junit annotation
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoSPringJpaProjectApplicationTests {
+	@Autowired
+	private TestRestTemplate testRestTemplate;
 
 	@Test
-	void contextLoads() {
+	public void testGetAllEmployees(){
+		//calling endpoint getEmployees
+		ResponseEntity<Employee[]> entity=testRestTemplate.getForEntity("/getEmployees",Employee[].class);
+
+		//System.out.println("--- Test :"+entity.getBody().length);
+		assertEquals(3,entity.getBody().length);
 	}
+
+	@Test
+	public  void testGetEmployeeById(){////getEmployeeByID/{empID}
+		ResponseEntity<Employee> entity=testRestTemplate.getForEntity("/getEmployeeByID/1",Employee.class);
+		assertEquals(1,entity.getBody().getId());
+	}
+
+
+
 
 }
